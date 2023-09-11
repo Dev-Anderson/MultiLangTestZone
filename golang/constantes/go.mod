c4daeb2f0fd3428c5fada7f13afad0f039c36693
Andesron da Silva <adersoosilvaa@gmail.com>
2023-09-11 15:44:42 -0300

    :sparkles: CRUD do banco de dados postgres

---
+++ golang/constantes/go.mod
@@ -0,0 +1,3 @@
+module constantes
+
+go 1.21.1
---
+++ golang/constantes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const a int = 10
+	const b int = 20
+	var area int
+
+	area = a * b
+	fmt.Println(area)
+}
