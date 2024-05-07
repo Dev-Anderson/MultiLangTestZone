5068997d4eaca793eae501574c58a926fdd45dd1
Anderson Silva <andersondasilva@MacBook-Air-de-Anderson.local>
2024-05-07 12:28:11 -0300

    api teste

--- golang/api-cookies-auth/handlers/handler_auth.go
+++ golang/api-cookies-auth/handlers/handler_auth.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"api-cookies/handlers/models"
 	"api-cookies/utils"
+	"encoding/json"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -27,23 +29,24 @@ type SessionData struct {
 }
 
 func (lac *LocalApiConfig) SignInHandler(c *gin.Context) {
-	var usertoAuth models.UserToAuth
-	if err := c.ShouldBindJSON(&usertoAuth); err != nil {
+	var userToAuth models.UserToAuth
+	if err := c.ShouldBindJSON(&userToAuth); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
-	//do validate here
-	validationErrors := utils.ValidateUserAuth(usertoAuth)
-	if len(validationErrors) > 0 {
+
+	// do validation here
+	validationErrors := utils.ValidateUserAuth(userToAuth)
+	if len(validationErrors) > 9 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": validationErrors,
 		})
 		return
 	}
 
-	//fetch  the user from database to match
+	//fetch the user from database to match
 	foundUser, err := lac.DB.FindUserByEmail(c, userToAuth.Email)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -51,10 +54,47 @@ func (lac *LocalApiConfig) SignInHandler(c *gin.Context) {
 		})
 		return
 	}
-	if foundUser.Password != usertoAuth.Password {
+	if foundUser.Password != userToAuth.Password {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
+
+	expirationTime := time.Now().Add(10 * time.Minute)
+	claims := &Claims{
+		Email: userToAuth.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// till here you are having token
+	sessionID := uuid.New().String()
+
+	//prepare the session data in redis
+	sessionData := map[string]interface{}{
+		"token":  tokenString,
+		"userId": foundUser.ID,
+	}
+
+	sessionDataJSON, err := json.Marshal(sessionData)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to encode the json data",
+		})
+		return
+	}
+
+	// saving the session data in redis
 }
